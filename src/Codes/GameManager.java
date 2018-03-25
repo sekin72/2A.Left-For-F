0aa77bcc47e73a4e79537d3910fa9501caa7e62d
omergeredeli <32222491+omergeredeli@users.noreply.github.com>
2018-03-26 02:12:36 +0300

    Add files via upload

--- src/Codes/GameManager.java
+++ src/Codes/GameManager.java
@@ -1,25 +1,30 @@
-import java.awt.Graphics;
-
-import javax.swing.ImageIcon;
-
-/**
- * @author Kaan
- *
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
+/**
+ *
+ * @author ömer
+ */
+import java.awt.Graphics;
 public class GameManager {
 
 	private Audio music;
 	private int currentLevel;
 	private Player player;
-	private Menu activeMenu = new MainMenu();
+	private Menu activeMenu;
 	public boolean gameOn = false;
-	public LevelControl levelController=null;
+	private LevelControl levelController;
 	public static GameManager Instance;
 
 	public GameManager() {
 		currentLevel = 1;
 		Instance=this;
+		 activeMenu = new MainMenu();
+		// music=defaultMusic();
+
 	}
 
 	public GameManager(int level) {
@@ -28,7 +33,7 @@ public class GameManager {
 	}
 
 	public void createNewLevel() {
-		player = new Player("Ali", new ImageIcon(".\\kahve.png").getImage());
+		player = new Player("Ali", null);
 		gameOn = true;
 		levelController = new LevelControl();
 	}
@@ -41,17 +46,32 @@ public class GameManager {
 			} 
 			else 
 			{
+                            
+				//createNewLevel();
+				//Menüler iþ yapacak burda
 			}
 		}
 	}
 
 	public void changeUI(String newMenu) {
+            
 		switch (newMenu) {
 		case "Options":
 			break;
 		case "Tutorials":
+                        activeMenu.setVisible(false);
+                        activeMenu = new Tutorial();
+                        activeMenu.setSize(690,320);
+                        
+                        activeMenu.setVisible(true);
 			break;
 		case "Credits":
+                        activeMenu.setVisible(false);
+                        activeMenu = new Credits();
+                        activeMenu.setSize(800,500);
+                        
+                        activeMenu.setVisible(true);
+                        
 			break;
 		case "CharacterSelection":
 			break;
@@ -60,20 +80,40 @@ public class GameManager {
 		case "Pause":
 			break;
 		case "MainMenu":
-			activeMenu.setVisible(false);
+                        activeMenu.setVisible(false);
 			activeMenu = new MainMenu();
-			activeMenu.setSize(810, 400);
-			activeMenu.setVisible(true);
+                        activeMenu.setSize(810,400);
+                        activeMenu.setVisible(true);
 			break;
 		case "Game":
-			activeMenu.setVisible(false);
-			activeMenu = new GameFrame(player);
-			activeMenu.setSize(750, 420);
-			activeMenu.setVisible(true);
+                        activeMenu.setVisible(false);
+                        activeMenu = new GameFrame();
+                        activeMenu.setSize(750,420);
+                        activeMenu.setVisible(true);
 			break;
+                case "Won":
+                        activeMenu.setVisible(false);
+                        activeMenu = new WinPage();
+                        activeMenu.setSize(750,400);
+                        activeMenu.setVisible(true);
+                        break;
+                case "Lost":
+                        activeMenu.setVisible(false);
+                        
+                        activeMenu = new LostPage();
+                        activeMenu.setSize(700,320);
+                        activeMenu.setVisible(true);
+                        break;
+                case "Battle":
+                        activeMenu.setVisible(false);
+                        activeMenu = new BattleScreen();
+                        activeMenu.setSize(800,500);
+                        
+                        activeMenu.setVisible(true);
+                        
 		default:
 			break;
 		}
-		System.gc();
+                System.gc();
 	}
 }
